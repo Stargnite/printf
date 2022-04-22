a06c7baf11397580aba4d87ab545dfd56bb691fd
Emmanuel Effiong <etokoffice@gmail.com>
2022-04-22 22:00:40 +0000

    parse_binary.c

--- parse_binary.c
+++ parse_binary.c
@@ -10,21 +10,22 @@
 
 int parse_binary(char *buff_dest, va_list arg, int buff_count)
 {
-	unsigned int bit, number;
-	int MSB = 0;
-	int i;
+	unsigned int number = va_arg(arg, unsigned int);
+	unsigned int tmp = number;
+	int bin = 1;
 
-	number = va_arg(arg, unsigned int);
-	for (i = INT_BITS; i > 0; i--)
+	while (tmp > 1)
 	{
-		bit = number >> (i - 1);
-		bit = bit & 1;
-		MSB = MSB | bit;
-		if (MSB != 0 || i == 1)
-		{
-			buff_dest[buff_count] = ('0' + bit);
-			buff_count++;
-		}
+		bin *= 2;
+		tmp /= 2;
+	}
+	tmp = number;
+	while (bin > 0)
+	{
+		buff_dest[buff_count] = ('0' + tmp / bin);
+		tmp %= bin;
+		bin /= 2;
+		buff_count++;
 	}
 	return (buff_count);
 }
