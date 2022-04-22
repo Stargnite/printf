99eea66cccc4a5ee1856983b4b203c0d8d83f4d3
Emmanuel Effiong <etokoffice@gmail.com>
2022-04-22 22:21:22 +0000

    parse_hex.c

--- parse_hex.c
+++ parse_hex.c
@@ -22,7 +22,40 @@ int parse_hex(char *buff_dest, va_list arg, int buff_count)
 	tmp = number;
 	while (hex > 0)
 	{
-		buff_dest[buff_count] = (tmp / hex + '0');
+		buff_dest[buff_count] = (tmp / hex < 9) ?
+			(tmp / hex + '0') : ('a' + tmp / hex - 10);
+		tmp %= hex;
+		hex /= 16;
+		buff_count++;
+	}
+	return (buff_count);
+}
+
+/**
+ * parse_X - sub %X by unsigned int arg num
+ * @buff_dest: string to change
+ * @arg: va_list arg to change
+ * @buff_count: index of buffer
+ * Return: new index
+ */
+
+int parse_X(char *buff_dest, va_list arg, int buff_count)
+{
+	unsigned int number = va_arg(arg, unsigned int);
+	unsigned int tmp = number;
+	int hex = 1;
+
+	while (tmp > 15)
+	{
+		hex *= 16;
+		tmp /= 16;
+	}
+
+	tmp = number;
+	while (hex > 0)
+	{
+		buff_dest[buff_count] = (tmp / hex < 9) ?
+			(tmp / hex + '0') : ('A' + tmp / hex - 10);
 		tmp %= hex;
 		hex /= 16;
 		buff_count++;
