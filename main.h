7d54633d35bc66b81b1036a559624f6484c0f65f
Emmanuel Effiong <etokoffice@gmail.com>
2022-05-03 20:29:45 +0000

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#define INT_BITS (4 * 8)
 
 #include <stdarg.h>
 #include <unistd.h>
@@ -9,7 +8,7 @@
 
 /**
  * struct call - struct call
- * @t: t - flag for data type
+ * @t: flag for data type
  * @f: function - associated function
  */
 
@@ -19,21 +18,17 @@ typedef struct call
 	int (*f)(char *, va_list, int);
 } call_t;
 
-int _print(const char *format, ...);
-int buff_append(char *buff_dest, va_list arg, int buff_count, char type);
-int print_buff(char *buff, unsigned int nbuff);
-int str_len(char *s);
-char *_strcpy(char *dest, char *src);
-int parse_char(char *buff_dest, va_list arg, int buff_count);
-int parse_string(char *buff_dest, va_list arg, int buff_count);
-int parse_int(char *buff_dest, va_list arg, int buff_count);
-int parse_mod(char *buff_dest, va_list arg, int buff_count);
-int parse_binary(char *buff_dest, va_list arg, int buff_count);
-int parse_oct(char *buff_dest, va_list arg, int buff_count);
-int parse_hex(char *buff_dest, va_list arg, int buff_count);
-int parse_uint(char *buff_dest, va_list arg, int buff_count);
-int parse_rev(char *buff_dest, va_list arg, int buff_count);
-int parse_X(char *buff_dest, va_list arg, int buff_count);
-int parse_R13(char *buff_dest, va_list arg, int buff_count);
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_c(va_list ar_list);
+int (*get_func(char s))(va_list ar_lsit);
+int print_s(va_list ar_list);
+int print_d(va_list ar_list);
+int print_i(va_list ar_list);
+int print_b(va_list ar_list);
+int print_u(va_list ar_list);
+int print_o(va_list ar_list);
+int print_x(va_list ar_list);
+int print_X(va_list ar_list);
 
 #endif
